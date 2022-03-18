082c1a6a8caf9c89d7eacf1bf54857a87c59ce09
Acetann <paulfremont27@gmail.com>
2022-03-18 22:41:11 +0100

    Ajout première liaison API

--- components/Users/Utilisateurs.tsx
+++ components/Users/Utilisateurs.tsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, ListRenderItemInfo, Text, View } from "react-native";
+import { FriendContainer } from "../Friends/FriendContainer";
 
 interface UsersProps{}
 
 export const Users: React.FunctionComponent<UsersProps> = () => {
+
+    const [data, setData] = useState([]);
+    const getMoviesFromApiAsync = async () => {
+        try {
+          const response = await fetch(
+            'http://192.168.1.12:3001/api/utilisateur'
+          );
+          const json = await response.json();
+          return setData(json);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+     
+       useEffect(() => {
+        getMoviesFromApiAsync();
+       }, []);
     return (
         <View>
-            <Text>
-                {"Users"}
-            </Text>
+            <FlatList
+                data={data}
+                renderItem={({ item }: ListRenderItemInfo<ItemType>) => (
+                    <FriendContainer name={item.pseudo} pseudo={item.nom} />
+                )}
+                keyExtractor={(item) => item.id}
+                />
         </View>
     )
 }
